e72072001f838026b902bf42ee08d8321c455848
Delarmelina <felipeferreiradelarmelina@gmail.com>
2022-01-07 07:06:25 -0300

    FrontEnd da tabela de colaboradores

--- frontend/src/pages/employees/employees.jsx
+++ frontend/src/pages/employees/employees.jsx
@@ -1,13 +1,45 @@
 import React from "react";
 
+import { getUserList } from '../../services/userMethods'
+
 import Content from '../../components/content'
 
 import * as FaIcon from "react-icons/fa";
 
 function Employees() {
 
+    const [tableElement, settableElement] = React.useState([]);
+
+    React.useEffect(() => {
+        getUserList().then(res => {
+            settableElement(res.map(user => {
+
+                console.log(user.email && '')
+
+                return (
+                    <tr key={user._id}>
+                        <td>{user.id}</td>
+                        <td>{user.name[0].toUpperCase() + user.name.slice(1).toLowerCase()}</td>
+                        <td>{user.email}</td>
+                        <td>{user.department}</td>
+                        <td><FaIcon.FaPen color="yellow" style={{ cursor: 'pointer' }}/></td>
+                        <td><FaIcon.FaTrash color="red" style={{ cursor: 'pointer' }}/></td>
+                    </tr>
+                )
+            }))
+        })
+    }, [])
+
     return (
         <Content title='Colaboradores' >
+            <div className="row mb-3 mt-1">
+                <div className="col-12">
+                    <button type='button' className="btn btn-block btn-sucess float-sm-right" style={{ width: '200px ', backgroundColor: 'lime' }}>
+                        <FaIcon.FaPlus className="mr-3 mb-1" />
+                        <span>Novo Colaborador</span>
+                    </button>
+                </div>
+            </div>
             <div className="row">
                 <div className="col-12">
                     <div className="card">
@@ -20,6 +52,25 @@ function Employees() {
                                     </div>
                                 </div>
                             </div>
+                        </div>
+                        <div className="card-body table-responsive p-0" style={{ height: '500px' }}>
+                            <table className="table table-head-fixed text-nowrap">
+                                <thead style={{ textAlign: 'center' }}>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Nome</th>
+                                        <th>E-mail</th>
+                                        <th>Departamento</th>
+                                        <th><FaIcon.FaPen /></th>
+                                        <th><FaIcon.FaUpload /></th>
+                                    </tr>
+                                </thead>
+                                <tbody style={{ textAlign: 'center' }}>
+                                    {
+                                        tableElement
+                                    }
+                                </tbody>
+                            </table>
                         </div>
                     </div>
                 </div>
